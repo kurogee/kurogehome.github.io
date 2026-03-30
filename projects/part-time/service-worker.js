d9df1ca7a032c62a03f8819fbb8e29c9c78b3db4
kurogee <null.blanker@gmail.com>
2026-03-30 20:53:06 +0900

    Upgrade PayBuddy

---
+++ projects/part-time/service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'paybuddy-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './PayBuddy_icon.png',
+  './PayBuddy_header.png'
+];
+
+// インストール時にキャッシュする
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// フェッチ時にキャッシュを返す、なければネットワークへ
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                // Ignore API requests or chrome-extension requests
+                if (event.request.url.startsWith('http') && event.request.method === 'GET') {
+                  cache.put(event.request, responseToCache);
+                }
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
